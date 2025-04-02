53f537057a9d7e7172c327a87235296f86eff67d
SamuelQuetin <quetin@abes.fr>
2025-04-02 11:37:21 +0200

    supp runbatch

--- docker/run_batch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i 's/PERISCOPE_BATCH_AT_STARTUP=0/PERISCOPE_BATCH_AT_STARTUP=1/' .env
-docker compose up -d periscope-batch
-sed -i 's/PERISCOPE_BATCH_AT_STARTUP=1/PERISCOPE_BATCH_AT_STARTUP=0/' .env
