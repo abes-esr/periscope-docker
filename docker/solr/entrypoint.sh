29222d0f64020f2548b57b6f87e21584f750a72b
SamuelQuetin <quetin@abes.fr>
2025-04-03 15:34:27 +0200

    Fix SolR dockerFile

--- docker/solr/entrypoint.sh
+++ docker/solr/entrypoint.sh
@@ -9,10 +9,14 @@ cp -r /opt/solr/server/solr/configsets/periscope-v2 /var/solr/data/configsets/pe
 if [ $? -eq 0 ]; then
   chown -R solr:solr /var/solr/data/configsets/periscope-v2
   chmod -R 755 /var/solr/data/configsets/periscope-v2
-  solr start -f
 else
   echo "Error copying configset"
   exit 1
 fi
 
-tail -f /var/log/solr.log
+#tail -f /var/log/solr.log
+
+solr start &
+sleep 5
+solr create_core -c periscope-v2 -d /var/solr/data/configsets/periscope-v2/conf -p 8983
+solr restart -f
